8bf40088f8805bfc2bbd8a65917aa80d3fc4ab1f
Sam Saccone <samccone@gmail.com>
2014-03-10 16:25:42 -0400

    split converter

--- ruby_converter.rb
+++ ruby_converter.rb
@@ -1,13 +1,32 @@
+#!/usr/bin/env ruby
+# Convert SASS/SCSS to Stylus
+# Initial work by Andrey Popp (https://github.com/andreypopp)
+
 require 'sass'
 
 class ToStylus < Sass::Tree::Visitors::Base
 
+  def self.convert(file)
+    options = Sass::Engine::DEFAULT_OPTIONS
+    engine = Sass::Engine.for_file(file, options)
+    tree = engine.to_tree
+    visit(tree)
+  end
+
   def visit(node)
-    method = "visit_#{node_name node}"
+    if self.class.respond_to? :node_name
+      method = "visit_#{node.class.node_name}"
+    else
+      method = "visit_#{node_name node}"
+    end
     if self.respond_to?(method, true)
       self.send(method, node) {visit_children(node)}
     else
-      raise "unhandled node: '#{node_name node}'"
+      if self.class.respond_to? :node_name
+        raise "unhandled node: '#{node.class.node_name}'"
+      else
+        raise "unhandled node: '#{node_name node}'"
+      end
     end
   end
 
@@ -61,6 +80,11 @@ class ToStylus < Sass::Tree::Visitors::Base
     emit "$#{node.name} = #{node.expr.to_sass}"
   end
 
+  def visit_mixindef(node)
+    emit "#{node.name}(#{render_args(node.args)})"
+    visit_children node
+  end
+
   def visit_media(node)
     emit "@media #{node.query.map{|i| i.is_a?(Sass::Script::Variable) ? i.inspect : i}.join}"
     visit_children node
@@ -68,11 +92,6 @@ class ToStylus < Sass::Tree::Visitors::Base
 
   def visit_content(node)
     emit '{block}'
-  end
-
-  def visit_mixindef(node)
-    emit "#{node.name}(#{render_args(node.args)})"
-    visit_children node
   end
 
   def visit_mixin(node)
@@ -99,4 +118,5 @@ class ToStylus < Sass::Tree::Visitors::Base
     visit_children(node)
     @lines.join("\n")
   end
+
 end
